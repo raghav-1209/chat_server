94289c28526d024ed06cf7b2be404ee31d667b7a
raghav-1209 <mangaliraghav@gmail.com>
2026-04-16 20:57:14 +0530

    modified test application

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -2,6 +2,9 @@ package com
 
 import io.ktor.client.request.*
 import io.ktor.http.*
+import io.ktor.server.config.MapApplicationConfig
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
 import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -10,11 +13,14 @@ class ApplicationTest {
 
     @Test
     fun testRoot() = testApplication {
+        environment {
+            config = MapApplicationConfig(
+                "ktor.environment" to "test"
+            )
+        }
+
         application {
             module()
-        }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
         }
     }
 
